a7ae1cf570896d57d409a2e6a7ffffc420254cfd
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-30 20:45:15 +0200

    Added schema model for User

---
+++ src/db/model/User.js
@@ -0,0 +1,38 @@
+const {Schema, model, SchemaTypes} = require {"mongoose"};
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  added: [
+    {
+      type: SchemaTypes.ObjectId,
+      ref: "Tattoo",
+      default: [],
+    },
+  ],
+  favourites: [
+    {
+      type: SchemaTypes.ObjectId,
+      ref: "Tattoo",
+      default: [],
+    },
+  ],
+});
+
+const User = model("User", UserSchema, "users");
+
+module.exports = User;
