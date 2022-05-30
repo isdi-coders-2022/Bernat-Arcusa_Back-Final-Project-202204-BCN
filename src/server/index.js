ce69c595488d66aff153f805a2c40ef35521e79d
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-30 21:51:48 +0200

    Added general error middleware

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
+const { generalError } = require("./middlewares/errors/errors");
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
+
+app.use(generalError);
 
 module.exports = app;
