d9264030b4e623c7e0178cb6226417c389528b20
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-02 11:47:22 +0200

    Added Joi middleware and register credentials schema

--- src/server/routers/usersRouters/usersRouters.js
+++ src/server/routers/usersRouters/usersRouters.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
+const { validate } = require("express-validation");
 const express = require("express");
 const {
   userLogin,
   userRegister,
 } = require("../../controllers/usersControllers/usersControllers");
+const credentialsRegisterSchema = require("../../schemas/userCredentialsSchema");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/login", userLogin);
 
-usersRouter.post("/register", userRegister);
+usersRouter.post(
+  "/register",
+  validate(credentialsRegisterSchema),
+  userRegister
+);
 
 module.exports = usersRouter;
