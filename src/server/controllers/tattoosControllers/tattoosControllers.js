9b83089eb36cb7ca41f3eddb555f19d0592ad846
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-05 16:04:42 +0200

    Added delete controller and route

--- src/server/controllers/tattoosControllers/tattoosControllers.js
+++ src/server/controllers/tattoosControllers/tattoosControllers.js
@@ -14,4 +14,22 @@ const getTattoos = async (req, res, next) => {
   }
 };
 
-module.exports = { getTattoos };
+const deleteTattoo = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deletedTattoo = await Tattoo.findByIdAndDelete(id);
+    if (deletedTattoo) {
+      res.status(202).json({ message: `Tattoo has been deleted` });
+    } else {
+      const error = new Error("Any tattoo to delete has found");
+      error.statusCode = 404;
+      error.customMessage = "Any tattoo to delete has found";
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getTattoos, deleteTattoo };
--- src/server/routers/tattoosRouters/tattoosRouters.js
+++ src/server/routers/tattoosRouters/tattoosRouters.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const {
   getTattoos,
+  deleteTattoo,
 } = require("../../controllers/tattoosControllers/tattoosControllers");
 
 const tattoosRouter = express.Router();
 
 tattoosRouter.get("/list", getTattoos);
+
+tattoosRouter.delete("/:id", deleteTattoo);
 
 module.exports = tattoosRouter;
