fb1e69b0e2435e26b72c8cc9dad830d16debc08d
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-10 13:30:05 +0200

    Deleted multer code, references and everything related

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,6 @@ const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
-app.use("/uploads/images", express.static("images"));
 app.use(express.json());
 app.use(helmet());
 
--- src/server/routers/tattoosRouters/tattoosRouters.js
+++ src/server/routers/tattoosRouters/tattoosRouters.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
 const {
   getTattoos,
   deleteTattoo,
@@ -9,12 +7,10 @@ const {
 } = require("../../controllers/tattoosControllers/tattoosControllers");
 const { auth } = require("../../middlewares/auth/auth");
 
-const upload = multer({ dest: path.join("uploads", "images") });
-
 const tattoosRouter = express.Router();
 
 tattoosRouter.get("/list", getTattoos);
 tattoosRouter.delete("/:id", auth, deleteTattoo);
-tattoosRouter.post("/newTattoo", auth, upload.single("image"), createTattoo);
+tattoosRouter.post("/newTattoo", auth, createTattoo);
 
 module.exports = tattoosRouter;
