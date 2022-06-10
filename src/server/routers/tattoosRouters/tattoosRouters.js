4f812d05962c3acec904b2fe8349690efe2b4dfc
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-10 16:45:52 +0200

    Added multer on create tattoo

--- src/server/routers/tattoosRouters/tattoosRouters.js
+++ src/server/routers/tattoosRouters/tattoosRouters.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const {
   getTattoos,
   deleteTattoo,
@@ -7,10 +9,12 @@ const {
 } = require("../../controllers/tattoosControllers/tattoosControllers");
 const { auth } = require("../../middlewares/auth/auth");
 
+const upload = multer({ dest: path.join("uploads", "images") });
+
 const tattoosRouter = express.Router();
 
 tattoosRouter.get("/list", getTattoos);
 tattoosRouter.delete("/:id", auth, deleteTattoo);
-tattoosRouter.post("/newTattoo", auth, createTattoo);
+tattoosRouter.post("/newTattoo", auth, upload.single("image"), createTattoo);
 
 module.exports = tattoosRouter;
